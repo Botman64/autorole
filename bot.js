e535cc33642eb286ab39e631e171a0b39b9b1c56
KamuiKody <cfurr17@gmail.com>
2025-03-05 04:28:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const guilds = require("./config.json");
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMembers
-] });
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ] 
+});
 
 client.on('ready', () => {
   console.log(`Launched as a bot: ${client.user.tag}!`);
 });
 
 client.on('guildMemberAdd', member => {
-  if (!guilds[member.guild.id]) return; 
-  return member.roles.add(member.guild.roles.cache.get(guilds[member.guild.id]));
+  const configRoles = guilds[member.guild.id];
+  if (!configRoles) return;
+  
+  if (Array.isArray(configRoles)) {
+    configRoles.forEach(roleId => {
+      member.roles.add(member.guild.roles.cache.get(roleId));
+    });
+  } else {  
+    member.roles.add(member.guild.roles.cache.get(configRoles));
+  }
 });
 
 client.login(process.env.BOT_TOKEN);

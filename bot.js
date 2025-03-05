8c69c6bd4b425c0480cd98df9a3cf2d1c3e294c3
Botman <62488881+Botman64@users.noreply.github.com>
2025-03-05 04:11:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
 const guilds = require("./config.json");
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMembers
-] });
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent
+  ] 
+});
 
 client.on('ready', () => {
   console.log(`Launched as a bot: ${client.user.tag}!`);
